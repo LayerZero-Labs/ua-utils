baf0e151caa84b15fd1b68fcdf1adb0868318a60
Ryan Goulding <goulding@layerzerolabs.org>
2023-11-27 20:23:03 -0800

    feat:  allow configuration of Gnosis contractAddresses
    Provide our own types as gnosis does not define adequate types.
    
    Signed-off-by: Ryan Goulding <goulding@layerzerolabs.org>

---
+++ src/utils/gnosis.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from 'fs'
+
+const GNOSIS_SAFE_FILE_ENCODING = 'utf-8'
+
+/**
+ * Gnosis Safe configuration for a specific network.
+ */
+type SafeConfig = {
+    safeAddress: string
+    url: string
+    contractNetworks?: ContractNetworks
+}
+
+/**
+ * Contract addresses for each network.
+ */
+type ContractNetworks = {
+    [chainListId: number]: {
+        multiSendAddress: string
+        safeMasterCopyAddress: string
+        safeProxyFactoryAddress: string
+    }
+}
+
+/**
+ * Converts a ContractNetworks object to a string.
+ * @param {ContractNetworks} contractNetworks The ContractNetworks object to convert.
+ */
+export const toContractNetworksString = (contractNetworks?: ContractNetworks): string => {
+    if (contractNetworks === undefined) {
+        return ''
+    }
+    return Object.entries(contractNetworks).reduce((accumulator, [chainListId, config]) => {
+        return accumulator + `contractNetworks[chainListId=${chainListId}, multiSendAddress=${config.multiSendAddress}, safeMasterCopyAddress=${config.safeMasterCopyAddress}, safeProxyFactoryAddress=${config.safeProxyFactoryAddress}]`;
+    }, '');
+}
+
+/**
+ * Gnosis Safe configuration per network.
+ */
+export interface SafeConfigs {
+    [chainName: string]: SafeConfig
+}
+
+/**
+ * Reads the safe config file and returns the parsed SafeConfigs.
+ * @param {string} fileName The name of the safe config file.
+ */
+export const getSafeConfigs = (fileName: string): SafeConfigs => {
+    return JSON.parse(readFileSync(fileName, GNOSIS_SAFE_FILE_ENCODING)) as SafeConfigs
+}
